4636dcbce6f64a6f2546449e04e1477c9c6bc37d
ChristosKrilisDev <christos.krilis.dev@gmail.com>
2022-01-22 22:41:09 +0200

    Character Controller

--- GGJ2022/Assets/Scripts/AudioManager.cs
+++ GGJ2022/Assets/Scripts/AudioManager.cs
@@ -4,167 +4,177 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+namespace GGJ.CK
 {
-
-    public Sound[] sounds;
-
-    public static AudioManager instance;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        if(instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
-
-        foreach(Sound s in sounds)
-        {
-            s.source = gameObject.AddComponent<AudioSource>();
-
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
-            s.source.playOnAwake = false;
-        }
-    }
-
-    //How to play sound
-    //FindObjectOfType<AudioManager>().Play("nameOfSound");
-
-    //Better - AudioManager.instance.Play();
-    //Even Better - Make it a thread
-
-    public void Play(string name) {
-
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)
-        {
-            Debug.LogWarning($"Problem playing sound with name: {s.name}");
-
-        }
-        else
-        {
-            s.source.Stop();
-            if (s.source.loop == false) s.source.PlayOneShot(s.source.clip);
-            else s.source.Play();
-        }
-        
-    }
-
-    public void Stop(string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-
-        if(s.source.isPlaying)StartCoroutine(StopGradually(s));
-
-        //s.source.Stop();
-    }
-
-    public bool IsPlaying(string name)
+    public class AudioManager : MonoBehaviour
     {
 
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        public Sound[] sounds;
 
-        if (s.source.isPlaying) return true;
+        public static AudioManager instance;
 
-        return false;
-
-    }
-
-    public void AbruptStop(string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if(s.source.isPlaying)s.source.Stop();
-    }
-
-    IEnumerator StopGradually(Sound s)
-    {
-
-        float startVolume = s.volume;
-        float step = 0.01f;
-
-        int numberIter = (int)((float)startVolume / ((float)step));
-        //Debug.Log($"NumberIter: {numberIter}");
-        for(int i = 0; i < numberIter; i++)
+        // Start is called before the first frame update
+        void Awake()
         {
-            s.volume -= step;
-            s.source.volume = s.volume;
-            yield return new WaitForSeconds(0.02f);
-        }
-
-        s.source.Stop();
-        s.volume = startVolume;
-        s.source.volume = startVolume;
-
-    }
-
-    public void SetVolume(float value, string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)
-        {
-            Debug.LogWarning($"Problem setting sound for: {s.name}");
-        }
-        else
-        {
-            s.volume = value;
-            s.source.volume = value;
-        }
-    }
-
-    public float GetVolume(string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)
-        {
-            return 1;
-        }
-        else
-        {
-            return s.source.volume;
-        }
-    }
-
-    //Attach source to specific gameObject if it doesnt have one and play sound
-
-    public void PlayOnGameObject(GameObject go, string name)
-    {
-
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)
-        {
-            Debug.LogWarning($"Problem playing sound with name: {s.name}");
-
-        }
-        else
-        {
-            AudioSource source;
-            if (go.GetComponent<AudioSource>() == null)
+            if(instance == null)
             {
-                source = go.AddComponent<AudioSource>();
-                source.clip = s.source.clip;
-                source.volume = s.source.volume;
-                source.pitch = s.source.pitch;
-                source.loop = s.source.loop;
-                source.spatialBlend = 0.8f;
-                source.playOnAwake = false;
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+
+            foreach(Sound s in sounds)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+
+                s.source.clip = s.clip;
+                s.source.volume = s.volume;
+                s.source.pitch = s.pitch;
+                s.source.loop = s.loop;
+                s.source.playOnAwake = false;
+            }
+        }
+
+        //How to play sound
+        //FindObjectOfType<AudioManager>().Play("nameOfSound");
+
+        //Better - AudioManager.instance.Play();
+        //Even Better - Make it a thread
+
+        public void Play(string name)
+        {
+
+            Sound s = Array.Find(sounds , sound => sound.name == name);
+            if(s == null)
+            {
+                Debug.LogWarning($"Problem playing sound with name: {s.name}");
+
+            }
+            else
+            {
+                s.source.Stop();
+                if(s.source.loop == false)
+                    s.source.PlayOneShot(s.source.clip);
+                else
+                    s.source.Play();
             }
 
-            else source = go.GetComponent<AudioSource>();
-
-            source.PlayOneShot(source.clip);
-            
-            //s.source.PlayOneShot(s.source.clip);
         }
 
+        public void Stop(string name)
+        {
+            Sound s = Array.Find(sounds , sound => sound.name == name);
+
+            if(s.source.isPlaying)
+                StartCoroutine(StopGradually(s));
+
+            //s.source.Stop();
+        }
+
+        public bool IsPlaying(string name)
+        {
+
+            Sound s = Array.Find(sounds , sound => sound.name == name);
+
+            if(s.source.isPlaying)
+                return true;
+
+            return false;
+
+        }
+
+        public void AbruptStop(string name)
+        {
+            Sound s = Array.Find(sounds , sound => sound.name == name);
+            if(s.source.isPlaying)
+                s.source.Stop();
+        }
+
+        IEnumerator StopGradually(Sound s)
+        {
+
+            float startVolume = s.volume;
+            float step = 0.01f;
+
+            int numberIter = (int)((float)startVolume / ((float)step));
+            //Debug.Log($"NumberIter: {numberIter}");
+            for(int i = 0; i < numberIter; i++)
+            {
+                s.volume -= step;
+                s.source.volume = s.volume;
+                yield return new WaitForSeconds(0.02f);
+            }
+
+            s.source.Stop();
+            s.volume = startVolume;
+            s.source.volume = startVolume;
+
+        }
+
+        public void SetVolume(float value , string name)
+        {
+            Sound s = Array.Find(sounds , sound => sound.name == name);
+            if(s == null)
+            {
+                Debug.LogWarning($"Problem setting sound for: {s.name}");
+            }
+            else
+            {
+                s.volume = value;
+                s.source.volume = value;
+            }
+        }
+
+        public float GetVolume(string name)
+        {
+            Sound s = Array.Find(sounds , sound => sound.name == name);
+            if(s == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return s.source.volume;
+            }
+        }
+
+        //Attach source to specific gameObject if it doesnt have one and play sound
+
+        public void PlayOnGameObject(GameObject go , string name)
+        {
+
+            Sound s = Array.Find(sounds , sound => sound.name == name);
+            if(s == null)
+            {
+                Debug.LogWarning($"Problem playing sound with name: {s.name}");
+
+            }
+            else
+            {
+                AudioSource source;
+                if(go.GetComponent<AudioSource>() == null)
+                {
+                    source = go.AddComponent<AudioSource>();
+                    source.clip = s.source.clip;
+                    source.volume = s.source.volume;
+                    source.pitch = s.source.pitch;
+                    source.loop = s.source.loop;
+                    source.spatialBlend = 0.8f;
+                    source.playOnAwake = false;
+                }
+
+                else
+                    source = go.GetComponent<AudioSource>();
+
+                source.PlayOneShot(source.clip);
+
+                //s.source.PlayOneShot(s.source.clip);
+            }
+
+        }
     }
 }
---
+++ GGJ2022/Assets/Scripts/Shader/OutLine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ.CK{
+
+
+public class OutLine : MonoBehaviour
+{
+    [SerializeField] private Material outlineMat;
+    [SerializeField] private float outlineScaleFactor;
+    [SerializeField] private Color outlineColor;
+    private Renderer outlineRender;
+
+
+    private void Start()
+    {
+        outlineRender = CreateOutline(outlineMat , outlineScaleFactor , outlineColor);
+    }
+
+    Renderer CreateOutline(Material outMat, float scaleFactor, Color color)
+    {
+        GameObject outlineObj = Instantiate(this.gameObject , transform.position , transform.rotation, transform);
+
+        Renderer rend = outlineObj.GetComponent<Renderer>();
+
+        rend.material = outlineMat;
+        rend.material.SetColor("_OutLineColor", color);
+        rend.material.SetFloat("_Scale",scaleFactor);
+        rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        outlineObj.GetComponent<OutLine>().enabled = false;
+        outlineObj.GetComponent<Collider>().enabled = false;
+
+        rend.enabled = false;
+
+        return rend;
+
+
+    }
+
+
+}
+}
